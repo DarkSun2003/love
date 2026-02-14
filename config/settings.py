15713d50e3f1bc81027561974fc6e32e4c642614
DarkSun2003 <damilareogunnowo10@gmail.com>
2026-02-14 16:08:24 +0100

    debug.2

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
+
 ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = []
 
